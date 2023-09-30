82e2ff58833b59266ad59075e2be369d5ba1077d
Suikan <26223147+suikan4github@users.noreply.github.com>
2023-10-01 07:33:43 +0900

    Refine the decoder class implementation

--- src/four2sevendecoder.cpp
+++ src/four2sevendecoder.cpp
@@ -1,7 +1,9 @@
 #include "four2sevendecoder.hpp"
 
 Four2SevenDecoder::Four2SevenDecoder(Common common, Logic logic)
-    : common_(common), logic_(logic) {}
+    : common_(common),  // Initialize class variable.
+      logic_(logic)     // Initialize class variable.
+{}
 
 uint32_t Four2SevenDecoder::decode(uint32_t code, bool period) {
   uint32_t segments = 0;
@@ -12,8 +14,10 @@ uint32_t Four2SevenDecoder::decode(uint32_t code, bool period) {
   // Set USB ( period pattern )
   if (period) segments |= 0x80;
 
+  // For anode common display, logic should be netative.
   if (common_ == anode) segments = ~segments;
 
+  // In the case of the negative logic LED driver circuit.
   if (logic_ == negative) segments = ~segments;
 
   return segments;
